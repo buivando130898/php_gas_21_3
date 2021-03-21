25feb0f49925ae4d78534313638bfc648b32633a
BuiDo <buivando130898@gmail.com>
2021-03-21 11:15:07 +0700

    update_index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Gas _Ether</title>
+        <meta charset="utf-8" />
+        <meta name="description" content="Gas _Ether" />
+        <meta name="keywords" content="Wordpress, theme wordpress, html, css, html css free, SEO, dịch vụ SEO, Thiết kế website chuẩn SEO">
+        <meta name="author" content="Tác giả" />
+        <meta http-equiv="Content-Language" content="Vi"> <!-- Khai bao ngôn ngữ -->
+        <meta http-equiv="refresh" content="30"> <!-- Làm mới tài liệu trong 30s -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">     <!-- Đặt chế độ xem  -->
+        <link rel="stylesheet" href="./style.css"> <!-- Thẻ nhúng file CSS -->
+        <script src="./javascript.js"></script>
+    
+    </head>
+
+
+
+<body>
+    
+</body>
+
+
+</html>

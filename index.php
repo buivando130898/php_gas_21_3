0932774003e7804de4989b0a34012d8d77256957
BuiDo <buivando130898@gmail.com>
2021-03-21 11:28:42 +0700

    update

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+ // connect mysql
+    $servername = "localhost";
+    $username = "iothello_datalock";
+    $password = "ngangoNGAO05";
+    $dbname = "iothello_datalock";
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+    
+
+    $sql = "SELECT * FROM login WHERE acc = '$user' and pass = '$password' ";
+    $query = mysqli_query($conn,$sql);
+    $num_rows = mysqli_num_rows($query);
+
+
+    $bienx = ['t2','t3','t4','t5','t6','t7','t8', 't10'];
+    $bieny = [3, 10, 400, 5, 22, 13, 20, 22];
+
+?>
+
+
+<script>
+
+    var bienx =  ['t2','t3','t4','t5','t6','t7','t8', 't10'];
+    var bieny = [3, 10, 40, 5, 22, 13, 20, 22, 66, 66,44,47,88,37];
+    var CHART = document.getElementById("line_chart").getContext('2d');
+    var line_chart = new Chart(CHART,{
+        type: 'line',
+        data:{
+            labels: <?php echo json_encode($bienx, JSON_NUMERIC_CHECK);   ?>,
+            datasets: [{
+                lablel: "name_data",
+                data: <?php echo json_encode($bieny, JSON_NUMERIC_CHECK);   ?>
+
+            }]
+        }
+    });
+
+
+
+</script>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,13 +55,21 @@
         <meta http-equiv="refresh" content="30"> <!-- Làm mới tài liệu trong 30s -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">     <!-- Đặt chế độ xem  -->
         <link rel="stylesheet" href="./style.css"> <!-- Thẻ nhúng file CSS -->
-        <script src="./javascript.js"></script>
+        <script src="./js.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>    
+ 
     
     </head>
 
 
 
 <body>
+
+    <div>
+        <canvas id="line_chart"></canvas>
+    </div>
+
+
     
 </body>
 
